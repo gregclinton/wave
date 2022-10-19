712f2d30ce026f8ba09e989edb84c9116ec8c322
Greg Clinton <greg.clinton@gmail.com>
2022-10-19 15:15:06 -0700

    latest

--- wave.js
+++ wave.js
@@ -3,10 +3,10 @@ const svgns = "http://www.w3.org/2000/svg";
 function line(e, x1, y1, x2, y2, color) {
     const line = document.createElementNS(svgns, 'line');
 
-    line.setAttributeNS(null, 'x1', x1);
-    line.setAttributeNS(null, 'y1', y1);
-    line.setAttributeNS(null, 'x2', x2);
-    line.setAttributeNS(null, 'y2', y2);
+    line.setAttributeNS(null, 'x1', scale.x(x1));
+    line.setAttributeNS(null, 'y1', scale.y(y1));
+    line.setAttributeNS(null, 'x2', scale.x(x2));
+    line.setAttributeNS(null, 'y2', scale.y(y2));
     line.setAttributeNS(null, 'stroke', color);
     line.setAttributeNS(null, 'stroke-width', 2);
     e.appendChild(line);
@@ -19,13 +19,23 @@ function clear(e) {
     }
 }
 
+let scale = {};
+
 function wave1(e, height, width) {
-    let x = 100;
+    let t = 0;
+
+    scale.x = x => x * width / (2 * Math.PI);
+    scale.y = y => -y + height / 2;
 
     setInterval(() => {
         clear(e);
-        line(e, x, 0, x + 100, height, 'darkblue');
-        x += 10;
+        let A = 120;
+        let step = 0.1;
+        let fn = x => A * Math.sin(x);
+
+        for (let x = 0; x < 2 * Math.PI; x += step) {
+            line(e, x, fn(x), x + step, fn(x + step), 'darkblue');
+        }
     }, 1000);
 }
 
