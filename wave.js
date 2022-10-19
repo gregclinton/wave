cead5680e2c61ae2f09a9a28f420cc71925fbd07
Greg Clinton <greg.clinton@gmail.com>
2022-10-19 14:41:12 -0700

    latest

--- wave.js
+++ wave.js
@@ -8,22 +8,31 @@ function line(e, x1, y1, x2, y2, color) {
     line.setAttributeNS(null, 'x2', x2);
     line.setAttributeNS(null, 'y2', y2);
     line.setAttributeNS(null, 'stroke', color);
-    line.setAttributeNS(null, 'stroke-width', 1);
+    line.setAttributeNS(null, 'stroke-width', 2);
     e.appendChild(line);
     return line;
 }
 
-const svg = document.getElementById('wave')
-const height = svg.getAttribute('height');
-const width = svg.getAttribute('width');
-
-let x = 100;
-
-setInterval(() => {
-    while (svg.lastChild) {
-        svg.removeChild(svg.lastChild);
+function clear(e) {
+    while (e.lastChild) {
+        e.removeChild(e.lastChild);
     }
+}
 
-    line(svg, x, 0, x + 100, height, 'darkblue');
-    x += 10;
-}, 1000);
+function wave1(e, height, width) {
+    let x = 100;
+
+    setInterval(() => {
+        clear(e);
+        line(e, x, 0, x + 100, height, 'darkblue');
+        x += 10;
+    }, 1000);
+}
+
+function wave() {
+    const svg = document.getElementById('wave')
+
+    wave1(svg, svg.getAttribute('height'), svg.getAttribute('width'));
+}
+
+wave();
